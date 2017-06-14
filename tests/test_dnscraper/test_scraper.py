32a9656167ef11c2262b08fe62fed24c953479c0
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-06-14 23:01:35 +0200

    Test parse method

--- tests/test_dnscraper/test_scraper.py
+++ tests/test_dnscraper/test_scraper.py
@@ -1,15 +1,47 @@
 import os
 import sys
 import unittest
+
 #Add module to PYTHONPATH for testing
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+import dnscraper.scraper as scraper
 
-import dnscraper
+
+FST_PAGE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "dn_b49_1.html")
+SND_PAGE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "dn_b49_2.html")
+LST_PAGE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "dn_b49_4.html")
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestScraper(unittest.TestCase):
+    def test_first_page_of_board(self):
+        with open(FST_PAGE, "rb") as fd:
+            next_page_url, threads = scraper.Board.parse(fd.read())
+        self.assertEqual(next_page_url, "/lifestyle/digital-world/pc-internet-smartphone_b49/seite-2/")
+        self.assertEqual(len(threads), 31)
+        titles, urls = zip(*threads)
+        # The first url is a pinned thread from another board.
+        for url in urls[1:]:
+            self.assertTrue(url.startswith("/lifestyle/digital-world/pc-internet-smartphone/"))
+
+    def test_second_page_of_board(self):
+        with open(SND_PAGE, "rb") as fd:
+            next_page_url, threads = scraper.Board.parse(fd.read())
+        self.assertEqual(next_page_url, "/lifestyle/digital-world/pc-internet-smartphone_b49/seite-3/")
+        self.assertEqual(len(threads), 31)
+        titles, urls = zip(*threads)
+        # The first url is a pinned thread from another board.
+        for url in urls[1:]:
+            self.assertTrue(url.startswith("/lifestyle/digital-world/pc-internet-smartphone/"))
+
+    def test_last_page_of_board(self):
+        with open(LST_PAGE, "rb") as fd:
+            next_page_url, threads = scraper.Board.parse(fd.read())
+        self.assertEqual(next_page_url, None)
+        self.assertEqual(len(threads), 8)
+        titles, urls = zip(*threads)
+        # The first url is a pinned thread from another board.
+        for url in urls[1:]:
+            self.assertTrue(url.startswith("/lifestyle/digital-world/pc-internet-smartphone/"))
 
 
 if __name__ == '__main__':
